73b53fcf55da2a462f15b5289106d6a5fa806ff2
Sheldon38 <109095852+Sheldon38@users.noreply.github.com>
2022-07-15 14:58:42 +0530

    Update imgrestore.m

--- imgrestore.m
+++ imgrestore.m
@@ -1,8 +1,7 @@
-%imgconvert
-fid = fopen('E:\clg work\Sem 4\Matlab Files\imgnew.txt', 'r');
+%code to covert the hex values back to original image
+fid = fopen('imgnew.txt', 'r');         %verilog output path here
 if fid == -1, error('Cannot open file'); end
-%ImgSize = fscanf(fid, '%d %d', 2);
-uselesscomments=textscan(fid,'%c',168);
+uselesscomments=textscan(fid,'%c',168);   
 ImgData = fscanf(fid, '%x ',Inf);
 Img     = reshape(ImgData,[256 384]);
 Img = cat(3, Img, Img, Img);
